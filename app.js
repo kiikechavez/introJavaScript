d19e8c9f01d9adbd8c193cd98e49760ac0c41c6a
Enrique Vergara <brandon.chavez@udea.edu.co>
2022-08-07 16:31:39 -0500

    Subiendo archivos a mi repositorio de JavaScript

---
+++ app.js
@@ -0,0 +1,14 @@
+let numero1 = 30;
+let numero2 = 50;
+
+let name = "Juan";
+let lastName = "Perez";
+
+let resultado = numero1 >= numero2;
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log(resultado);
